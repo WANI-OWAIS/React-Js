6fb1ae9c7745a1494d7b709b6f180bc0b0e1e0ff
WANI-OWAIS <waniowais528@gmail.com>
2025-02-04 22:48:35 +0530

    useState

---
+++ 03useState/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [val,setVal] = useState({name: 'John', isBanned: false})
+  return (
+    <div className='p-6'>
+      <h1>Name: {val.name}</h1>
+      <h2>Banned: {val.isBanned.toString()}</h2>
+      <button onClick={()=>setVal({...val, isBanned: !val.isBanned})} className='px-3 py-1 rounded-full bg-blue-400 text-white'>Change</button>
+    </div>
+  )
+}
+
+export default App
