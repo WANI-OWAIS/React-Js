41ca7323faf0a4c4ce508df1b91647e52704d56e
WANI-OWAIS <waniowais528@gmail.com>
2025-02-08 15:18:46 +0530

    reactrouting

--- 08ReactRouting/src/App.jsx
+++ 08ReactRouting/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Nav from './components/Nav'
+import Routing from './utils/Routing'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Nav/>
+      <Routing/>
+    </>
   )
 }
 
---
+++ 08ReactRouting/src/components/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div className=' w-1/2 m-auto mt-10 p-5'>
+        <h1 className='text-red-200 text-5xl mb-2'>User</h1>
+        <p className='mb-2 text-zinc-500'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla reiciendis pariatur, nemo facere facilis fugit necessitatibus, a qui nesciunt temporibus accusamus, cum culpa inventore enim eligendi beatae minima eos laboriosam.
+            </p>
+        <button className='px-3 py-1 bg-zinc-200 rounded-full'>Explore More</button>
+    </div>
+  )
+}
+
+export default User
